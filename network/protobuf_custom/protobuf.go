b2e7288bb30714fd9bc020313fd68db9b6f2bec2
tangs <tangsong1988kaka@gmail.com>
2020-08-07 15:44:01 +0800

    log.

--- network/protobuf_custom/protobuf.go
+++ network/protobuf_custom/protobuf.go
@@ -205,7 +205,7 @@ func (p *Processor) Unmarshal(data []byte) (interface{}, error) {
 
 // goroutine safe
 func (p *Processor) Marshal(msg interface{}) ([][]byte, error) {
-
+	log.Debug("Marshal data:%v", msg)
 	if msgBase, ok := msg.(*MessageBase); ok {
 		idx := 0
 		header := make([]byte, 256)
