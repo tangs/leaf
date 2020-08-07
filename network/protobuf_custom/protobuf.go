ff72754821ffac672d59365f17fb7ccb76472951
tangs <tangsong1988kaka@gmail.com>
2020-08-07 17:09:35 +0800

    update.

--- network/protobuf_custom/protobuf.go
+++ network/protobuf_custom/protobuf.go
@@ -220,6 +220,7 @@ func (p *Processor) Marshal(msg interface{}) ([][]byte, error) {
 			idx += binary.PutUvarint(header[idx:], uint64(len(msg.Cmd)))
 			idx += copy(header[idx:], msg.Cmd)
 
+			log.Debug("Marshal inner msg:%v", header[:idx], msg.Bytes)
 			return [][]byte{header[:idx], msg.Bytes}, nil
 		} else {
 			idx += binary.PutVarint(header[idx:], int64(msgBase.SerialId))
